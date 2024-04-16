eb985f3cdca637fbd545fcf1b7372b195982cf46
barbarahellmann <babshellmann@web.de>
2024-04-16 11:40:39 +0200

    addded bonus 1

--- src/main.ts
+++ src/main.ts
@@ -28,15 +28,21 @@ const student3: Student = {
     grades: ["C",undefined,undefined,2, "A"]
 }
 
+
+// Bonus
+function charLength(student:Student): number {
+    return student.firstName.length + student.lastName.length + 4
+    }
+
+
+
 // Step 1: Write a function that outputs a student with their grades, where a student should be output with first name, last name, age, and a list of grades.
-
-
-
 
 function studentOutput(student: Student): void {
     console.log(student.firstName + " " + student.lastName + " (" + student.age + ")")
-    for (let i:number = 0; i <10; i++) {console.log("=")}  // 30 sind zu viel
+    for (let i:number = 0; i <charLength(student); i++) {console.log("=")}  // 30 sind zu viel
     console.log("Noten: " + student.grades.map(grade => grade === undefined ? '*' : grade))
+    console.log("Notendurchschnitt: ")
     console.log(" ")
 }
 
